4ba8e57277d0d171bf5669b64d5d782d72d72fbd
Dattaraj Dhadas <dhadas808@yahoo.com>
2023-11-13 20:41:34 +0530

    added the create Restaurant entity

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,29 +1,8 @@
-import { ArgsType, Field, InputType, OmitType} from "@nestjs/graphql";
+import { ArgsType, Field, InputType, OmitType,PickType} from "@nestjs/graphql";
 import {IsString, IsBoolean} from 'class-validator'
 import { Restaurant } from "../entities/restaurant.entity";
 
-@InputType()
-export class CreateRestaurantDto extends OmitType(Restaurant, ['id'], InputType){}
-// @ArgsType()
-// export class CreateRestaurantDto{
-//     @Field(type => String)
-//     @IsString()
-//     name : string;
-
-//     @Field(type => Boolean)
-//     @IsBoolean()
-//     isVegan : boolean;
-
-
-//     @Field(type => String)
-//     @IsString()
-//     address : string;
-
-//     @Field(type => String)
-//     @IsString()
-//     ownersName : string;
-
-//     @Field(type=> String)
-//     @IsString()
-//     categoryName : string
-// }
+// @InputType()
+// export class CreateRestaurantDto {}
+@ArgsType()
+export class CreateRestaurantDto extends PickType(Restaurant,["name", "isVegan", "address","ownersName","categoryName"]){}
