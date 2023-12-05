4b8bfec91d9f4daa8fe72bae269180fdef7a4ab9
Dattaraj Dhadas <87413639+DataRaj@users.noreply.github.com>
2023-12-05 20:30:19 +0530

    Update user.entity.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,48 +1,82 @@
-import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
-import * as bcrypt from "bcryptjs" 
-import { BeforeInsert, Column, Entity } from "typeorm";
-import { UserRole } from "../enums/userRole.enum";
-import { CoreEntity } from "src/common/entities/core.entity";
-import { InternalServerErrorException } from "@nestjs/common";
-import { IsEmail, IsEnum, IsString } from "class-validator";
+import { Field, InputType, ObjectType, registerEnumType } from '@nestjs/graphql';
+import { Entity, Column, BeforeInsert, BeforeUpdate, OneToMany } from 'typeorm';
+import { IsBoolean, IsEmail, IsEnum, IsString } from 'class-validator';
+import { CoreEntity } from '../../common/entities/core.entity';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
+import { Restaurant } from '../../restaurants/entities/restaurant.entity';
+import { Order } from 'src/orders/entities/order.entity';
+import { Payment } from 'src/payments/entities/payment.entity';
 
-registerEnumType(UserRole, {name : 'UserRole'})
+// El enum me mapeara en mi base de datos estos roles como [0, 1, 2]
+export enum UserRole {
+	Client = 'Client',
+	Owner = 'Owner',
+	Delivery = 'Delivery',
+}
 
-@InputType({isAbstract: true})
+// Creo un enum para GraphQL
+registerEnumType(UserRole, { name: 'UserRole' });
+
+@InputType('UserInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
-export class User extends CoreEntity{
+export class User extends CoreEntity {
+	@Field(() => String)
+	@Column({ unique: true })
+	@IsEmail()
+	email: string;
 
-    @Column()
-    @Field(type => String)
-    @IsEmail()
-    email:string
+	@Field(() => String)
+	@Column({ select: false })
+	@IsString()
+	password: string;
 
-    @Column()
-    @Field(type => String)
-    @IsString()
-    password:string 
+	// Este UserRole que le defino al @Field de GrapQL es el que cree con registerEnumType
+	@Field(() => UserRole)
+	@Column({ type: 'enum', enum: UserRole })
+	@IsEnum(UserRole)
+	role: UserRole;
+   
+	@Field(() => Boolean)
+	@Column({ default: false })
+	@IsBoolean()
+	verified: boolean;
 
-    @Column({type : 'enum', enum : UserRole})
-    @Field(type => UserRole)
-    @IsEnum(UserRole)
-    role : UserRole
+	@Field(() => [Restaurant])
+	@OneToMany(() => Restaurant, restaurant => restaurant.owner)
+	restaurants: Restaurant[];
 
-    @BeforeInsert()
-    async hashpassword(): Promise<void>{
-        try{
-            this.password = await  bcrypt.hash(this.password, 10);
-        } catch(e){
-            console.log(e)
-            throw new InternalServerErrorException();
-        }
-    }
-    async checkPassword(aPassword: string): Promise<boolean> {
-        try{
-            return await bcrypt.compare(aPassword,this.password);
-        }catch(e){
-            console.log(e)
-            throw new InternalServerErrorException();
-        }
-    }
+	@Field(() => [Order])
+	@OneToMany(() => Order, order => order.customer)
+	orders: Order[];
+
+	@Field(() => [Order])
+	@OneToMany(() => Order, order => order.driver)
+	rides: Order[];
+
+	@Field(() => [Payment])
+	@OneToMany(() => Payment, payment => payment.user, { eager: true })
+	payments: Payment[];
+
+	@BeforeInsert()
+	@BeforeUpdate()
+	async hashPassword(): Promise<void> {
+		// Si existe un password en el objeto que estoy salvando entonces es que hago el hash
+		if (this.password) {
+			try {
+				this.password = await bcrypt.hash(this.password, 10);
+			} catch (error) {
+				throw new InternalServerErrorException();
+			}
+		}
+	}
+
+	async checkPassword(aPassword: string): Promise<boolean> {
+		try {
+			return await bcrypt.compare(aPassword, this.password);
+		} catch (error) {
+			throw new InternalServerErrorException();
+		}
+	}
 }
