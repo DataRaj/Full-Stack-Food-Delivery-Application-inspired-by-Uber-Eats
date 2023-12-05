be097533e07c772391998d3c8fcf65ecdb394bc8
Dattaraj Dhadas <87413639+DataRaj@users.noreply.github.com>
2023-12-05 20:32:48 +0530

    Delete src directory

--- src/auth/role.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { UserRole } from '../users/enums/userRole.enum';
-
-export type AllowedRoles = keyof typeof UserRole | 'Any';
-export const Role = (roles: AllowedRoles[]) => SetMetadata('roles', roles);
--- src/restaurants/dtos/create-restaurant.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ArgsType, Field, InputType, OmitType,PickType} from "@nestjs/graphql";
-import {IsString, IsBoolean} from 'class-validator'
-import { Restaurant } from "../entities/restaurant.entity";
-
-// @InputType()
-// export class CreateRestaurantDto {}
-@ArgsType()
-export class CreateRestaurantDto extends PickType(Restaurant,["name", "isVegan", "address","ownersName","categoryName"]){}
